967e2de461f181cf60d1f03506dbe7333f25bc8f
Manuel  Lara <mlara@atsistemas.com>
2020-02-24 10:33:19 +0100

    remove spring cloud dependency

--- src/main/java/tfg/microservice/user/UserApplication.java
+++ src/main/java/tfg/microservice/user/UserApplication.java
@@ -3,12 +3,10 @@ package tfg.microservice.user;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class UserApplication {
 
 	public static void main(String[] args) {
